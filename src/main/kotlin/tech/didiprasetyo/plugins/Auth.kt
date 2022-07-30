42348435c7de9f6b45bd7212c6b5ef1daf309709
didi <didiprasetyo360@gmail.com>
2022-07-30 15:41:42 +0700

    create jwt authentication

---
+++ src/main/kotlin/tech/didiprasetyo/plugins/Auth.kt
@@ -0,0 +1,48 @@
+package tech.didiprasetyo.plugins
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
+import org.koin.ktor.ext.inject
+import org.slf4j.LoggerFactory
+import tech.didiprasetyo.domain.service.AuthService
+import java.util.*
+
+fun Application.configureJWT() {
+    val authService by inject<AuthService>()
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
+
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = myRealm
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(secret))
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .build()
+            )
+            validate { credential ->
+                LoggerFactory.getILoggerFactory().getLogger("jwt")
+                    .info("session id : ${credential.payload.getClaim("session")}")
+                val sessionId = credential.payload.getClaim("session").asString()
+                val session = authService.getSessionById(UUID.fromString(sessionId))
+                if (session != null){
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+            challenge { defaultScheme, realm ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
+        }
+    }
+}
