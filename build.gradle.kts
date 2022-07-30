3a1fc9f4e0ef7d44493056e59cfa417fc07bae23
didi <didiprasetyo360@gmail.com>
2022-07-30 15:23:12 +0700

    add koin and test and koin test junit 5 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
@@ -58,8 +59,11 @@ dependencies {
     implementation("org.mindrot:jbcrypt:0.4")
     // Apache Email
     implementation("org.apache.commons:commons-email:1.5")
+    // Faker
+    implementation("io.github.serpro69:kotlin-faker:1.11.0")
 
-    testImplementation("io.github.serpro69:kotlin-faker:1.11.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("io.insert-koin:koin-test:$koin_version")
+    implementation("io.insert-koin:koin-test-junit5:$koin_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
 }
