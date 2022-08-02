9219183ddd8ae59a7e687e0d08f4dc10bfb07616
didi <didiprasetyo360@gmail.com>
2022-08-02 12:44:49 +0700

    update auth service

--- src/main/kotlin/tech/didiprasetyo/domain/service/AuthService.kt
+++ src/main/kotlin/tech/didiprasetyo/domain/service/AuthService.kt
@@ -68,7 +68,7 @@ class AuthService(
             val token = emailToken.sign(user.email)
 
             // send email verification
-            sendVerifyEmail(user.email, token)
+            sendVerifyEmail(user, token)
             Response(
                 status = Status.Success,
                 message = "success register new account",
@@ -104,7 +104,7 @@ class AuthService(
         return sessionRepository.getById(id)
     }
 
-    suspend fun verifyEmail(token: String) {
+    suspend fun verifyEmail(token: String): Boolean {
         try {
             // verify token
             val verify = emailToken.verifier.verify(token)
@@ -116,14 +116,17 @@ class AuthService(
             val user = userRepository.getByEmail(email) ?: throw IllegalArgumentException("email now found")
             val now = System.currentTimeMillis() / 1000
             userRepository.update(user.copy(verifiedAt = now))
+            return true
         } catch (e: JWTVerificationException) {
             println("error : ${e.message}")
+            return false
         } catch (e: IllegalArgumentException) {
             println("error : ${e.message}")
+            return false
         }
     }
 
-    suspend fun deleteEmail(token: String) {
+    suspend fun deleteEmail(token: String): Boolean {
         try {
             // verify token
             val verify = emailToken.verifier.verify(token)
@@ -136,24 +139,29 @@ class AuthService(
             if (user.verifiedAt == null){
                 userRepository.delete(user)
             }
+            return true
         } catch (e: JWTVerificationException) {
             println("error : ${e.message}")
+            return false
         } catch (e: IllegalArgumentException) {
             println("error : ${e.message}")
+            return false
         }
     }
 
     private fun createUserToken(user: UserEntity, session: String): String {
+        val verified = user.verifiedAt != null
         return JWT.create()
             .withAudience(audience)
             .withIssuer(issuer)
             .withClaim("user", user.id.toString())
+            .withClaim("verified", verified)
             .withClaim("session", session)
             .withExpiresAt(Date(System.currentTimeMillis() + 1000 * 3600))
             .sign(Algorithm.HMAC256(secret))
     }
 
-    private fun sendVerifyEmail(toEmail: String, token: String) {
+    private fun sendVerifyEmail(user: UserRegister, token: String) {
         val email = HtmlEmail()
 //        val hostName = config.property("").getString()
         email.hostName = "smtp.mailtrap.io"
@@ -161,16 +169,293 @@ class AuthService(
         email.setAuthentication("08311fb1f4ad18", "5662f1b73a11f0")
         email.setFrom("noreply@mykos.tech")
         email.subject = "verify-email"
-        email.setHtmlMsg(
-            "\n" +
-                    "<a href=\"http://127.0.0.1/verify-email/$token\">link verify</a><br>\n" +
-                    "\n" +
-                    "<a href=\"http://127.0.0.1/delete-email/$token\">link delete account</a>\n" +
-                    "\n"
-        )
+        email.setHtmlMsg(setHtmlVerifyEmail(user.name, token))
         email.setTextMsg("Your email client does not support HTML messages")
-        email.addTo(toEmail)
+        email.addTo(user.email)
         email.send()
     }
 
+    private fun setHtmlVerifyEmail(name: String, token: String):String {
+        return "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "\n" +
+                "  <meta charset=\"utf-8\">\n" +
+                "  <meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\">\n" +
+                "  <title>Email Confirmation</title>\n" +
+                "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                "  <style type=\"text/css\">\n" +
+                "  /**\n" +
+                "   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.\n" +
+                "   */\n" +
+                "  @media screen {\n" +
+                "    @font-face {\n" +
+                "      font-family: 'Source Sans Pro';\n" +
+                "      font-style: normal;\n" +
+                "      font-weight: 400;\n" +
+                "      src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');\n" +
+                "    }\n" +
+                "\n" +
+                "    @font-face {\n" +
+                "      font-family: 'Source Sans Pro';\n" +
+                "      font-style: normal;\n" +
+                "      font-weight: 700;\n" +
+                "      src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');\n" +
+                "    }\n" +
+                "  }\n" +
+                "\n" +
+                "  /**\n" +
+                "   * Avoid browser level font resizing.\n" +
+                "   * 1. Windows Mobile\n" +
+                "   * 2. iOS / OSX\n" +
+                "   */\n" +
+                "  body,\n" +
+                "  table,\n" +
+                "  td,\n" +
+                "  a {\n" +
+                "    -ms-text-size-adjust: 100%; /* 1 */\n" +
+                "    -webkit-text-size-adjust: 100%; /* 2 */\n" +
+                "  }\n" +
+                "\n" +
+                "  /**\n" +
+                "   * Remove extra space added to tables and cells in Outlook.\n" +
+                "   */\n" +
+                "  table,\n" +
+                "  td {\n" +
+                "    mso-table-rspace: 0pt;\n" +
+                "    mso-table-lspace: 0pt;\n" +
+                "  }\n" +
+                "\n" +
+                "  /**\n" +
+                "   * Better fluid images in Internet Explorer.\n" +
+                "   */\n" +
+                "  img {\n" +
+                "    -ms-interpolation-mode: bicubic;\n" +
+                "  }\n" +
+                "\n" +
+                "  /**\n" +
+                "   * Remove blue links for iOS devices.\n" +
+                "   */\n" +
+                "  a[x-apple-data-detectors] {\n" +
+                "    font-family: inherit !important;\n" +
+                "    font-size: inherit !important;\n" +
+                "    font-weight: inherit !important;\n" +
+                "    line-height: inherit !important;\n" +
+                "    color: inherit !important;\n" +
+                "    text-decoration: none !important;\n" +
+                "  }\n" +
+                "\n" +
+                "  /**\n" +
+                "   * Fix centering issues in Android 4.4.\n" +
+                "   */\n" +
+                "  div[style*=\"margin: 16px 0;\"] {\n" +
+                "    margin: 0 !important;\n" +
+                "  }\n" +
+                "\n" +
+                "  body {\n" +
+                "    width: 100% !important;\n" +
+                "    height: 100% !important;\n" +
+                "    padding: 0 !important;\n" +
+                "    margin: 0 !important;\n" +
+                "  }\n" +
+                "\n" +
+                "  /**\n" +
+                "   * Collapse table borders to avoid space between cells.\n" +
+                "   */\n" +
+                "  table {\n" +
+                "    border-collapse: collapse !important;\n" +
+                "  }\n" +
+                "\n" +
+                "  a {\n" +
+                "    color: #1a82e2;\n" +
+                "  }\n" +
+                "\n" +
+                "  img {\n" +
+                "    height: auto;\n" +
+                "    line-height: 100%;\n" +
+                "    text-decoration: none;\n" +
+                "    border: 0;\n" +
+                "    outline: none;\n" +
+                "  }\n" +
+                "  </style>\n" +
+                "\n" +
+                "</head>\n" +
+                "<body style=\"background-color: #e9ecef;\">\n" +
+                "\n" +
+                "  <!-- start preheader -->\n" +
+                "  <div class=\"preheader\" style=\"display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;\">\n" +
+                "    A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.\n" +
+                "  </div>\n" +
+                "  <!-- end preheader -->\n" +
+                "\n" +
+                "  <!-- start body -->\n" +
+                "  <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+                "\n" +
+                "    <!-- start logo -->\n" +
+                "    <tr>\n" +
+                "      <td align=\"center\" bgcolor=\"#e9ecef\">\n" +
+                "        <!--[if (gte mso 9)|(IE)]>\n" +
+                "        <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\">\n" +
+                "        <tr>\n" +
+                "        <td align=\"center\" valign=\"top\" width=\"600\">\n" +
+                "        <![endif]-->\n" +
+                "        <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
+                "          <tr>\n" +
+                "            <td align=\"center\" valign=\"top\" style=\"padding: 36px 24px;\">\n" +
+                "              <a href=\"https://sendgrid.com\" target=\"_blank\" style=\"display: inline-block;\">\n" +
+                "                <img src=\"https://i.postimg.cc/Nf2tPHRp/Group-43.png\" alt=\"Logo\" border=\"0\" width=\"48\" style=\"display: block; width: 120px; max-width: 120px; min-width: 48px;\">\n" +
+                "              </a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </table>\n" +
+                "        <!--[if (gte mso 9)|(IE)]>\n" +
+                "        </td>\n" +
+                "        </tr>\n" +
+                "        </table>\n" +
+                "        <![endif]-->\n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "    <!-- end logo -->\n" +
+                "\n" +
+                "    <!-- start hero -->\n" +
+                "    <tr>\n" +
+                "      <td align=\"center\" bgcolor=\"#e9ecef\">\n" +
+                "        <!--[if (gte mso 9)|(IE)]>\n" +
+                "        <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\">\n" +
+                "        <tr>\n" +
+                "        <td align=\"center\" valign=\"top\" width=\"600\">\n" +
+                "        <![endif]-->\n" +
+                "        <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
+                "          <tr>\n" +
+                "            <td align=\"left\" bgcolor=\"#ffffff\" style=\"padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;\">\n" +
+                "              <h1 style=\"margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;\">Confirm Your Email Address</h1>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </table>\n" +
+                "        <!--[if (gte mso 9)|(IE)]>\n" +
+                "        </td>\n" +
+                "        </tr>\n" +
+                "        </table>\n" +
+                "        <![endif]-->\n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "    <!-- end hero -->\n" +
+                "\n" +
+                "    <!-- start copy block -->\n" +
+                "    <tr>\n" +
+                "      <td align=\"center\" bgcolor=\"#e9ecef\">\n" +
+                "        <!--[if (gte mso 9)|(IE)]>\n" +
+                "        <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\">\n" +
+                "        <tr>\n" +
+                "        <td align=\"center\" valign=\"top\" width=\"600\">\n" +
+                "        <![endif]-->\n" +
+                "        <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
+                "\n" +
+                "          <!-- start copy -->\n" +
+                "          <tr>\n" +
+                "            <td align=\"left\" bgcolor=\"#ffffff\" style=\"padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;\">\n" +
+                "              <p style=\"font-size: 20px;\">Hi $name,</p>\n" +
+                "              <p style=\"margin: 0;\">Tap the button below to confirm your email address.</p>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "          <!-- end copy -->\n" +
+                "\n" +
+                "          <!-- start button -->\n" +
+                "          <tr>\n" +
+                "            <td align=\"left\" bgcolor=\"#ffffff\">\n" +
+                "              <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+                "                <tr>\n" +
+                "                  <td align=\"center\" bgcolor=\"#ffffff\" style=\"padding: 12px;\">\n" +
+                "                    <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n" +
+                "                      <tr>\n" +
+                "                        <td align=\"center\" bgcolor=\"#1a82e2\" style=\"border-radius: 6px;\">\n" +
+                "                          <a href=\"http://127.0.0.1/verify-email/$token\" target=\"_blank\" style=\"display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;\">Verify Email Address</a>\n" +
+                "                        </td>\n" +
+                "                      </tr>\n" +
+                "                    </table>\n" +
+                "                  </td>\n" +
+                "                </tr>\n" +
+                "              </table>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "          <!-- end button -->\n" +
+                "          \n" +
+                "          <!-- start copy -->\n" +
+                "          <tr>\n" +
+                "            <td align=\"left\" bgcolor=\"#ffffff\" style=\"padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;\">\n" +
+                "              <p style=\"margin: 0;\">Did you receive this email without signing up? <a href=\"http://127.0.0.1/delete-email/$token\">click here</a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "          <!-- end copy -->\n" +
+                "\n" +
+                "          <!-- start copy -->\n" +
+                "          <tr>\n" +
+                "            <td align=\"left\" bgcolor=\"#ffffff\" style=\"padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;\">\n" +
+                "              <p style=\"margin: 0;\">If that doesn't work, copy and paste the following link in your browser:</p>\n" +
+                "              <p style=\"margin: 0;\"><a href=\"https://sendgrid.com\" target=\"_blank\">https://same-link-as-button.url/xxx-xxx-xxxx</a></p>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "          <!-- end copy -->\n" +
+                "\n" +
+                "          <!-- start copy -->\n" +
+                "          <tr>\n" +
+                "            <td align=\"left\" bgcolor=\"#ffffff\" style=\"padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf\">\n" +
+                "              <p style=\"margin: 0;\">Cheers,<br> Paste</p>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "          <!-- end copy -->\n" +
+                "\n" +
+                "        </table>\n" +
+                "        <!--[if (gte mso 9)|(IE)]>\n" +
+                "        </td>\n" +
+                "        </tr>\n" +
+                "        </table>\n" +
+                "        <![endif]-->\n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "    <!-- end copy block -->\n" +
+                "\n" +
+                "    <!-- start footer -->\n" +
+                "    <tr>\n" +
+                "      <td align=\"center\" bgcolor=\"#e9ecef\" style=\"padding: 24px;\">\n" +
+                "        <!--[if (gte mso 9)|(IE)]>\n" +
+                "        <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\">\n" +
+                "        <tr>\n" +
+                "        <td align=\"center\" valign=\"top\" width=\"600\">\n" +
+                "        <![endif]-->\n" +
+                "        <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
+                "\n" +
+                "          <!-- start permission -->\n" +
+                "          <tr>\n" +
+                "            <td align=\"center\" bgcolor=\"#e9ecef\" style=\"padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;\">\n" +
+                "              <p style=\"margin: 0;\">You received this email because we received a request for [type_of_action] for your account. If you didn't request [type_of_action] you can safely delete this email.</p>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "          <!-- end permission -->\n" +
+                "\n" +
+                "          <!-- start unsubscribe -->\n" +
+                "          <tr>\n" +
+                "            <td align=\"center\" bgcolor=\"#e9ecef\" style=\"padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;\">\n" +
+                "              <p style=\"margin: 0;\">To stop receiving these emails, you can <a href=\"https://sendgrid.com\" target=\"_blank\">unsubscribe</a> at any time.</p>\n" +
+                "              <p style=\"margin: 0;\">Paste 1234 S. Broadway St. City, State 12345</p>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "          <!-- end unsubscribe -->\n" +
+                "\n" +
+                "        </table>\n" +
+                "        <!--[if (gte mso 9)|(IE)]>\n" +
+                "        </td>\n" +
+                "        </tr>\n" +
+                "        </table>\n" +
+                "        <![endif]-->\n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "    <!-- end footer -->\n" +
+                "\n" +
+                "  </table>\n" +
+                "  <!-- end body -->\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>\n"
+    }
 }
